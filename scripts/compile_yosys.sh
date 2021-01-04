a8d164d5978d417ab02a1ddc1b0576be29b1f6c1
umarcor <unai.martinezcorral@ehu.eus>
2021-01-04 06:51:57 +0100

    print

--- scripts/compile_yosys.sh
+++ scripts/compile_yosys.sh
@@ -36,6 +36,11 @@ then
     else
         GHDL_LDLIBS="$PACKAGE_DIR/$NAME/lib/libghdl.a $(tr -s '\n' ' ' < $PACKAGE_DIR/$NAME/lib/libghdl.link)"
     fi
+
+    echo "$GHDL_LIBS"
+    echo "$PACKAGE_DIR/$NAME/lib/libghdl.link"
+    cat "$PACKAGE_DIR/$NAME/lib/libghdl.link"
+    exit 1
 fi
 
 _ghdl_conf() {
